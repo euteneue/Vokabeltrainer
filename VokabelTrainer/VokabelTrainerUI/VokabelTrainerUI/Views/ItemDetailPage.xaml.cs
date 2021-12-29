f7596737f84a971025dd0c3c810c7a1edf0ca6ee
Sven Euteneuer <sven_nospam@euteneuer.com>
2021-12-29 12:09:30 +0100

    Created Xamarin Forms UI

---
+++ VokabelTrainer/VokabelTrainerUI/VokabelTrainerUI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using VokabelTrainerUI.ViewModels;
+using Xamarin.Forms;
+
+namespace VokabelTrainerUI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ VokabelTrainer/VokabelTrainerUI/VokabelTrainerUI/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VokabelTrainerUI.Models;
+using VokabelTrainerUI.ViewModels;
+using VokabelTrainerUI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VokabelTrainerUI.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
